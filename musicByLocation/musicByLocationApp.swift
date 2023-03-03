67438a869ac35c945e2ad057500560a297d3af46
jakotiaha <jakotiah.a@etoncollege.org.uk>
2023-03-03 21:45:45 +0000

    Initial Commit

---
+++ musicByLocation/musicByLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicByLocationApp.swift
+//  musicByLocation
+//
+//  Created by Akshat Jakotiah on 03/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct musicByLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
